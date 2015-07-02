0cf6c1158285aa72e46ad1e69509e1fbae42e270
ferreret <ferreret@gmail.com>
2015-07-02 17:27:12 +0200

    Primer programa de ejemplo de java

---
+++ Introductions.java
@@ -0,0 +1,12 @@
+import java.io.Console;
+ 
+public class Introductions {
+  
+    public static void main(String[] args) {
+        Console console = System.console();
+        // Welcome to the Introductions program!  Your code goes below here
+        String firstName = "Nicolas"; // this is an example of camelcase
+        console.printf("Hello, my name is %s\n", firstName);
+        console.printf("%s is learning how to write Java\n", firstName);
+  }
+}
