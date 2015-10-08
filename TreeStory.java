437c7d85312221c67586e30cb621830083320a4f
ferreret <ferreret@gmail.com>
2015-10-08 11:26:37 +0200

    Fin ejemplo TreeStory

--- TreeStory.java
+++ TreeStory.java
@@ -8,12 +8,21 @@ public class TreeStory {
             noun - Person, place or thing
             verb - An action
             adjective - A description used to modify or describe a noun
-            Enter your amazing code here!
-        */
+            Enter your amazing code here! */
 
-        String name = console.readLine("Enter your name:   ");
+        /*  String name = console.readLine("Enter your name:   ");
         String adjective  = console.readLine("Enter an adjective:   ");
-        console.printf("%s is very %s\n", name, adjective);
-    }
-    
+        console.printf("%s is very %s\n", name, adjective); */
+
+        // __Name__is a __adjective__ __noun__. They are always __adverb__ __verb__.
+        String name      = console.readLine("Enter a name: ");
+        String adjective = console.readLine("Enter an adjective: ");
+        String noun      = console.readLine("Enter a noun: ");
+        String adverb    = console.readLine("Enter an adjective: ");
+        String verb      = console.readLine("Enter a verb ending in -ing; ");
+
+        console.printf("Your TreeStory: \n--------------\n");
+        console.printf("%s is a %s %s. ", name, adjective, noun);
+        console.printf("They are always %s %s. \n", adverb, verb);                
+    }   
 }
