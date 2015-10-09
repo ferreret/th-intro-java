a09e9f98b309c2235a445a8d36cd66fc5c087311
ferreret <ferreret@gmail.com>
2015-10-09 12:20:11 +0200

    Fin del curso básico de Java

--- TreeStory.java
+++ TreeStory.java
@@ -3,26 +3,43 @@ import java.io.Console;
 public class TreeStory {
     
     public static void main(String[] args) {
-        Console console = System.console();
-        /*  Some terms:
-            noun - Person, place or thing
-            verb - An action
-            adjective - A description used to modify or describe a noun
-            Enter your amazing code here! */
 
-        /*  String name = console.readLine("Enter your name:   ");
-        String adjective  = console.readLine("Enter an adjective:   ");
-        console.printf("%s is very %s\n", name, adjective); */
+        Console console = System.console();
 
         // __Name__is a __adjective__ __noun__. They are always __adverb__ __verb__.
+
+        String ageAsString = console.readLine("How old are you?  ");
+
+        int age = Integer.parseInt(ageAsString);
+
+        if (age < 13) { 
+            // Insert exit code
+            console.printf("Sorry you must be, at least 13 to use this program. \n");
+            System.exit(0);
+        }
+
         String name      = console.readLine("Enter a name: ");
         String adjective = console.readLine("Enter an adjective: ");
-        String noun      = console.readLine("Enter a noun: ");
+        String noun;
+        boolean isInvalidWord;
+
+        do {            
+            noun          = console.readLine("Enter a noun: ");
+            isInvalidWord = noun.equalsIgnoreCase("dork") ||
+                                    noun.equalsIgnoreCase("jerk") || 
+                                    noun.equalsIgnoreCase("nerd");
+
+            if (isInvalidWord) {
+                console.printf("Than language is not allowed. Try again \n\n");            
+            }
+        } while (isInvalidWord);
+
         String adverb    = console.readLine("Enter an adjective: ");
         String verb      = console.readLine("Enter a verb ending in -ing; ");
 
         console.printf("Your TreeStory: \n--------------\n");
         console.printf("%s is a %s %s. ", name, adjective, noun);
         console.printf("They are always %s %s. \n", adverb, verb);                
+
     }   
 }
